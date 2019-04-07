d89b9046ff024820d220cf663ba3617e826e2b59
Sim Yong Seng <simyseng@gmail.com>
2019-04-07 18:15:49 +0700

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Sim Yong Seng on 7/4/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
