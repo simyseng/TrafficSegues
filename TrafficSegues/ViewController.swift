095c3b248bfc6adbbd5c38efaa54d75b74baa4a0
Sim Yong Seng <simyseng@gmail.com>
2019-04-07 21:31:59 +0700

    Reset to new

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -10,18 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func unwindToRed (unwindSegue: UIStoryboardSegue) {
-        
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = textField.text
     }
 }
 
