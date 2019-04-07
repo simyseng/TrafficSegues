587fd10318d7dea15c2f66433c59a2e104b29c1c
Sim Yong Seng <simyseng@gmail.com>
2019-04-07 21:47:29 +0700

    Programmatic segues completed.

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueSwitch: UISwitch!
+    @IBAction func yellowButtonTapped(_ sender: UIButton) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    @IBAction func greenButtonTapped(_ sender: UIButton) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
